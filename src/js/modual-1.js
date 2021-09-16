412d49b77ce01d693f3436dcf3db32d71aa7ab64
NikitaMelnychencko <hunter6059@gmail.com>
2021-09-16 22:44:46 +0300

    fix

--- src/js/modual-1.js
+++ src/js/modual-1.js
@@ -1,47 +1,10 @@
-const refs = {
-  globalLink: document.querySelector('body'),
-  blockBtn: document.querySelector('.block-btn')
-  
-}
-
-const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
-];
-
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+const delay = ms => {
+  // Твой код
 };
 
-const colorPick = {
-  remind() {
-    this.timeoutID = undefined;
-  },
-  setup() {
-    if (typeof this.timeoutID === 'number') {
-      this.cancel();
-    }
-    this.timeoutID = setInterval(this.addBackGroundColorBody, 1000);
-  },
-  cancel() {
-    clearInterval(this.timeoutID);
-  },
-  addBackGroundColorBody() {
-    const randomValue = randomIntegerFromInterval(0, colors.length - 1);
-    refs.globalLink.style.backgroundColor = colors[randomValue]; 
-  }
-};
+const logger = time => console.log(`Resolved after ${time}ms`);
 
-refs.blockBtn.addEventListener('click', e => {
-  if (e.target.dataset.action === "start") {
-    colorPick.setup()
-    e.target.disabled = true
-  } else {
-    colorPick.cancel()
-    e.currentTarget.firstElementChild.disabled = false
-  }
-})
+// Вызовы функции для проверки
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
